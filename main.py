ec1db644e7446ac3539e0b974ee9a81a8ac61122
dawit101 <130009377+dawit101@users.noreply.github.com>
2023-04-06 05:39:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Math Functions
+
+#to turn all code into comments fast use: ctrl + /
+
+print(round(3.1)) #rounds down
+print(round(3.9)) #rounds up
+
+print(abs(-23)) #abs = return absolute value of argument, change all negative numebers to positive
